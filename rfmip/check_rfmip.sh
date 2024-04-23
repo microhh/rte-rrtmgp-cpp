8cf877ac24713171f959f02b6b8273f7a8429e86
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2024-04-23 15:18:31 +0200

    Add failure threshold.

--- rfmip/check_rfmip.sh
+++ rfmip/check_rfmip.sh
@@ -2,5 +2,5 @@
 python3 rfmip_init.py
 python3 rfmip_run.py
 python3 compare-to-reference.py --ref_dir ../rrtmgp-data/examples/rfmip-clear-sky/reference --tst_dir . \
---var rld rlu rsd rsu --file r??_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc
+--var rld rlu rsd rsu --file r??_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc --failure_threshold=7e-4
 
