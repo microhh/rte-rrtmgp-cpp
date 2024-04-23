7f8074af48557c96edc3da85be9da80674cedde3
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2024-04-23 15:11:52 +0200

    Create shell script to test rfmip.

---
+++ rfmip/check_rfmip.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+python3 rfmip_init.py
+python3 rfmip_run.py
+python3 compare-to-reference.py --ref_dir ../rrtmgp-data/examples/rfmip-clear-sky/reference --tst_dir . \
+--var rld rlu rsd rsu --file r??_Efx_RTE-RRTMGP-181204_rad-irf_r1i1p1f1_gn.nc
+
